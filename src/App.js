20d5487685c02f52b84497e2c6068a579e96adce
Slava Kovyazin <slava.kovazin@gmail.com>
2020-07-27 17:08:20 +0500

    feat: install material ui

--- src/App.js
+++ src/App.js
@@ -1,3 +1,11 @@
 import React from 'react'
 
-export const App = () => <h1>Hello world!</h1>
+import Button from '@material-ui/core/Button'
+
+export const App = () => {
+  return (
+    <Button color="primary" variant="contained">
+      Click me!
+    </Button>
+  )
+}
